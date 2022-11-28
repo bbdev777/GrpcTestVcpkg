7072cfda884d18bc816ff01bf006102f3a384759
bbdev1 <alexey.yakovlev.job@yandex.ru>
2022-11-28 15:30:42 +0300

    first commit

---
+++ install_vcpkg_grpc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y zip unzip
+sudo apt install -y build-essential pkg-config
+
+wget -O vcpkg.tar.gz https://github.com/microsoft/vcpkg/archive/master.tar.gz
+sudo mkdir /opt/vcpkg
+sudo tar xf vcpkg.tar.gz --strip-components=1 -C /opt/vcpkg
+sudo /opt/vcpkg/bootstrap-vcpkg.sh
+sudo ln -s /opt/vcpkg/vcpkg /usr/local/bin/vcpkg
+vcpkg version
+
+sudo /opt/vcpkg integrate install
+sudo /opt/vcpkg install grpc
