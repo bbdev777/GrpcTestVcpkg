3e4e949a308410115dd8f3e4555a08526caed878
bbdev1 <alexey.yakovlev.job@yandex.ru>
2022-11-28 18:22:37 +0300

    Realigned tabs

--- TestClient/main.cpp
+++ TestClient/main.cpp
@@ -9,28 +9,28 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	printf("TestGrpcClient v1.0\n");
-	
-	TestGrpcService::TestGrpcConverter	converter("localhost:50000");
+    printf("TestGrpcClient v1.0\n");
+    
+    TestGrpcService::TestGrpcConverter	converter("localhost:50000");
 
-	try
-	{
-		int		testValue = 12345;
+    try
+    {
+	int testValue = 12345;
 
-		std::string stringValue = converter.ConvertIntToString(testValue);
+	std::string stringValue = converter.ConvertIntToString(testValue);
 
-		printf("Source number %d\n", testValue);
-		printf("Result string %s\n", stringValue.c_str());
-	}
-	catch (std::exception& e)
-	{
-		printf("Error message: %s\n", e.what());
-	}
+	printf("Source number %d\n", testValue);
+	printf("Result string %s\n", stringValue.c_str());
+    }
+    catch (std::exception& e)
+    {
+	printf("Error message: %s\n", e.what());
+    }
 
 #ifdef WIN32
-	printf("Press Enter to exit\n");
-	int value = getchar();
+    printf("Press Enter to exit\n");
+    int value = getchar();
 #endif // WIN32
 
-	return 0; 
+    return 0; 
 }
